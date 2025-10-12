623dcfc0fb3871e6fc6fca4ee880057b5ce9600c
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2025-10-11 23:11:57 -0300

    feat: initial commit

---
+++ src/main/java/com/bspicinini/catalog/admin/CatalogAdminApplication.java
@@ -0,0 +1,13 @@
+package com.bspicinini.catalog.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bspicinini/catalog/admin/CatalogAdminApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bspicinini.catalog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogAdminApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
