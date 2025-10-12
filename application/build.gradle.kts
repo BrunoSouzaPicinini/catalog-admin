29743424826cc5c525fd99916c65e83e10b7e305
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2025-10-11 23:39:27 -0300

    refactor: upgrade gradlew to 9.1.0

---
+++ application/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+}
+
+group = "com.bspicinini.catalog.admin.application"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Adicione dependências específicas de application aqui
+}
